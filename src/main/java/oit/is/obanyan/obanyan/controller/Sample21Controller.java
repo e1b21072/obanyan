b2467bc63b205eb220f847735d0339ad6e7f4f76
e1b21115 <119831972+e1b21115@users.noreply.github.com>
2023-10-03 15:46:34 +0900

    sample2-1

---
+++ src/main/java/oit/is/obanyan/obanyan/controller/Sample21Controller.java
@@ -0,0 +1,16 @@
+package oit.is.obanyan.obanyan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Sample21Controller {
+  @GetMapping("/sample21")
+  public String sample21() {
+    return "sample21.html";
+  }
+}
