c671357e5518d7961ffc0c35c9271fd67da93fb2
e1b21072 <e1b21072@oit.ac.jp>
2023-10-03 14:35:35 +0900

    first commit

---
+++ src/main/java/oit/is/obanyan/obanyan/ObanyanApplication.java
@@ -0,0 +1,13 @@
+package oit.is.obanyan.obanyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObanyanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObanyanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/obanyan/obanyan/ObanyanApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.obanyan.obanyan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObanyanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
