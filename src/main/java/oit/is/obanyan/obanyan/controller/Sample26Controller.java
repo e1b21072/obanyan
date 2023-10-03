cbe505ec5a37fc188f3418deee5945cdd033bcd6
sho037 <103867061+sho037@users.noreply.github.com>
2023-10-03 16:52:39 +0900

    最小値から最大値までの合計をけいさんできるように

--- src/main/java/oit/is/obanyan/obanyan/controller/Sample26Controller.java
+++ src/main/java/oit/is/obanyan/obanyan/controller/Sample26Controller.java
@@ -1,5 +1,7 @@
 package oit.is.obanyan.obanyan.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class Sample26Controller {
   @GetMapping
   public String sample26() {
+    return "sample26.html";
+  }
+
+  @PostMapping
+  public String sample27(@RequestParam Integer min, @RequestParam Integer max, ModelMap model) {
+    int sum = 0;
+    ArrayList<Integer> sumRange = new ArrayList<>();
+    // minからmaxまでの数値を加算し，sumに代入する
+    // 同時にminからmaxまでの数値を順にArrayListに格納する
+    for (int i = min; i <= max; i++) {
+      sum = sum + i;
+      sumRange.add(i);
+    }
+    model.addAttribute("sumRange", sumRange);
+    model.addAttribute("sum", sum);
     return "sample26.html";
   }
 }
